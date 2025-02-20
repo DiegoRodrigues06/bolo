2bd66755d440310c886013714dda1fe3e3141bc5
DiegoRodrigues06 <diegodash06@gmail.com>
2025-02-20 20:23:02 -0300

    first commit

---
+++ LTDO/Main.java
@@ -0,0 +1,9 @@
+package LTDO;
+
+public class Main {
+    public static void main(String[] args) {
+    // Criando o objeto Bolo (bolo pronto)
+    Bolo bolo = new Bolo("Chocolate", 20);
+    bolo.exibirDetalhes();
+    }
+}
